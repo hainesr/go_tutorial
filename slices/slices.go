0a154cf844bb5f0fbb346b61919ac2cb17d7c1ae
Robert Haines <rhaines@manchester.ac.uk>
2017-09-19 16:51:55 +0100

    Slices exercise.

---
+++ slices/slices.go
@@ -0,0 +1,21 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+  y := make([][]uint8, dy)
+
+  for i := range y {
+    y[i] = make([]uint8, dx)
+
+    for j := range y[i] {
+      y[i][j] = uint8(i + j)
+    }
+  }
+
+  return y
+}
+
+func main() {
+  pic.Show(Pic)
+}
